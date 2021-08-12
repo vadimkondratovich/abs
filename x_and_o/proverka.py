774560b1be4304394c40a69c0f005398f13e82b3
Vadim <vadim.kondratovich1993@gmail.com>
2021-08-12 17:28:26 +0300

    added_files

--- x_and_o/proverka.py
+++ x_and_o/proverka.py
@@ -1,36 +1,47 @@
 import pygame as pg
 import sys
  
+
+W = 300
+H = 200
+
+
 WHITE = (255, 255, 255)
-RED = (225, 0, 50)
-GREEN = (0, 225, 0)
-BLUE = (0, 0, 225)
- 
-sc = pg.display.set_mode((400, 300))
-sc.fill(WHITE)
+BLACK = (0, 0, 0)
+GRAY = (125, 125, 125)
+LIGHT_BLUE = (64, 128, 255)
+GREEN = (0, 200, 64)
+YELLOW = (225, 225, 0)
+PINK = (230, 50, 230)
+
+
+sc = pg.display.set_mode((300, 200))
+
+pg.draw.rect(sc, (255, 255, 255), 
+                 (20, 20, 100, 75))
+pg.draw.rect(sc, (64, 128, 255), 
+                 (150, 20, 100, 75), 8)
+
+
+pg.draw.circle(sc, YELLOW, 
+                   (100, 100), 50)
+pg.draw.circle(sc, PINK, 
+                   (200, 100), 50, 10)
+
+
+
 pg.display.update()
- 
+
+
+
 while 1:
     for i in pg.event.get():
         if i.type == pg.QUIT:
             sys.exit()
-        if i.type == pg.MOUSEBUTTONDOWN:
-            if i.button == 1:
-                pg.draw.circle(
-                    sc, RED, i.pos, 20)
-                pg.display.update()
-            elif i.button == 3:
-                pg.draw.circle(
-                    sc, BLUE, i.pos, 20)
-                pg.draw.rect(
-                    sc, GREEN,
-                    (i.pos[0] - 10,
-                     i.pos[1] - 10,
-                     20, 20))
-                pg.display.update()
-            elif i.button == 2:
-                sc.fill(WHITE)
-                pg.display.update()
- 
-    pg.time.delay(20)
- 
+    pg.display.update()
+
+   
+    pg.time.delay(100)
+
+if __name__ == "__main()__":
+    main()
